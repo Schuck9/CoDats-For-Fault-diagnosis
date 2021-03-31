da34ff0af0b955ae513ff4ff0d948e856d964b80
Schuck9 <34061849+Schuck9@users.noreply.github.com>
2021-03-31 23:38:47 +0800

    Add files via upload

--- datasets/JNU2tfrecord.py
+++ datasets/JNU2tfrecord.py
@@ -70,9 +70,29 @@ def to_numpy(value):
 #         data = json.load(file_obj)
 #     return data
 
+def subdata_split():
+    main_dataset = ["jnu"]
+    # Transfer_dataset = ["12DriveEndFault"]
+    Hez = ["0","1","2"]
+    sub_size = ["700","600","500","400","300","200","100"]
+    output_dir = os.path.join("datasets", "tfrecords")
+    isSubset = True
+    adaptation_problems = []
+    for name in main_dataset:
+        for hz in Hez:
+            if isSubset:
+                for size in sub_size:
+                    adaptation_problems.append(name + "_" +hz+"_"+size+".tfrecord")
+            else:
+                    adaptation_problems.append(name +"_" +hz+".tfrecord")
+    print("names generated!")
+
+    
+
+
 
 if __name__ == '__main__':
-    dataset_name = "JNU"
+    dataset_name = "jnu"
     data_dir = "D:\\2020BUAA\dataset\JNU"
     write_dir = "D:\\2020BUAA\dataset\JNU\\tfrecords"
     npy_data = os.path.join(data_dir,"JNU_data_0-2.npy")
@@ -104,24 +124,64 @@ if __name__ == '__main__':
         with open(pic_data, 'rb') as file_1:
             txt_all_data = pickle.load(file_1)
             
+
     source_train_X,source_train_y = txt_all_data[0]
     source_val_X,source_val_y  = txt_all_data[1]
     target_train_X,target_train_y = txt_all_data[2]
     target_val_X,target_val_y = txt_all_data[3]
 
 
-    source_train_X = np.reshape(source_train_X,(source_train_X.shape[0],source_train_X.shape[1],1))
-    source_val_X = np.reshape(source_val_X,(source_val_X.shape[0],source_val_X.shape[1],1))
-    target_train_X = np.reshape(target_train_X,(target_train_X.shape[0],target_train_X.shape[1],1))
-    target_val_X = np.reshape(target_val_X,(target_val_X.shape[0],target_val_X.shape[1],1))
+    main_dataset = ["jnu"]
+    # Transfer_dataset = ["12DriveEndFault"]
 
-    source_train_write_filename = os.path.join(write_dir,tfrecord_filename(dataset_name,str(transfer_task[0][0])+"_train"))
-    source_val_write_filename = os.path.join(write_dir,tfrecord_filename(dataset_name,str(transfer_task[0][0])+"_valid"))
-    target_val_write_filename = os.path.join(write_dir,tfrecord_filename(dataset_name,str(transfer_task[1][0])+"_valid"))
-    target_train_write_filename = os.path.join(write_dir,tfrecord_filename(dataset_name,str(transfer_task[1][0])+"_train"))
+    sub_size = ["700","600","500","400","300","200","100"]
+    output_dir = os.path.join("datasets", "tfrecords")
+    isSubset = True
+    adaptation_problems = []
+    if isSubset:
+        for size in sub_size:
+            subset_name_1 = str(transfer_task[0][0])+"_"+size
+            subset_name_2 = str(transfer_task[1][0])+"_"+size
+            train_size = int(size)
+            val_size = int(train_size*1.0/len(source_train_X)*len(source_val_X))
+            source_train_X,source_train_y = source_train_X[:train_size], source_train_y[:train_size]
+            source_val_X,source_val_y  = source_val_X[:val_size], source_val_y[:val_size]
+            target_train_X,target_train_y = target_train_X[:train_size],target_train_y[:train_size]
+            target_val_X,target_val_y = target_val_X[:val_size],target_val_y[:val_size]
 
-    write(source_train_write_filename,source_train_X,source_train_y)
-    write(source_val_write_filename,source_val_X,source_val_y)
-    write(target_train_write_filename,target_train_X,target_train_y)
-    write(target_val_write_filename,target_val_X,target_val_y)
-    print("data to tfrecord done!")
+            source_train_write_filename = os.path.join(write_dir,tfrecord_filename(dataset_name,subset_name_1+"_train"))
+            source_val_write_filename = os.path.join(write_dir,tfrecord_filename(dataset_name,subset_name_1+"_valid"))
+            target_val_write_filename = os.path.join(write_dir,tfrecord_filename(dataset_name,subset_name_2+"_valid"))
+            target_train_write_filename = os.path.join(write_dir,tfrecord_filename(dataset_name,subset_name_2+"_train"))
+
+            source_train_X = np.reshape(source_train_X,(source_train_X.shape[0],source_train_X.shape[1],1))
+            source_val_X = np.reshape(source_val_X,(source_val_X.shape[0],source_val_X.shape[1],1))
+            target_train_X = np.reshape(target_train_X,(target_train_X.shape[0],target_train_X.shape[1],1))
+            target_val_X = np.reshape(target_val_X,(target_val_X.shape[0],target_val_X.shape[1],1))
+
+
+            write(source_train_write_filename,source_train_X,source_train_y)
+            write(source_val_write_filename,source_val_X,source_val_y)
+            write(target_train_write_filename,target_train_X,target_train_y)
+            write(target_val_write_filename,target_val_X,target_val_y)
+            print("data to tfrecord done!")
+    else:
+            adaptation_problems.append(name +"_" +str(hz[0]))
+            source_train_write_filename = os.path.join(write_dir,tfrecord_filename(dataset_name,str(transfer_task[0][0])+"_train"))
+            source_val_write_filename = os.path.join(write_dir,tfrecord_filename(dataset_name,str(transfer_task[0][0])+"_valid"))
+            target_val_write_filename = os.path.join(write_dir,tfrecord_filename(dataset_name,str(transfer_task[1][0])+"_valid"))
+            target_train_write_filename = os.path.join(write_dir,tfrecord_filename(dataset_name,str(transfer_task[1][0])+"_train"))
+            
+
+
+            source_train_X = np.reshape(source_train_X,(source_train_X.shape[0],source_train_X.shape[1],1))
+            source_val_X = np.reshape(source_val_X,(source_val_X.shape[0],source_val_X.shape[1],1))
+            target_train_X = np.reshape(target_train_X,(target_train_X.shape[0],target_train_X.shape[1],1))
+            target_val_X = np.reshape(target_val_X,(target_val_X.shape[0],target_val_X.shape[1],1))
+
+
+            write(source_train_write_filename,source_train_X,source_train_y)
+            write(source_val_write_filename,source_val_X,source_val_y)
+            write(target_train_write_filename,target_train_X,target_train_y)
+            write(target_val_write_filename,target_val_X,target_val_y)
+            print("data to tfrecord done!")
