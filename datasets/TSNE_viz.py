59691f538109a576b1cda6bb33c9da72991df14e
Schuck9 <34061849+Schuck9@users.noreply.github.com>
2021-04-01 23:26:36 +0800

    Add files via upload

---
+++ datasets/TSNE_viz.py
@@ -0,0 +1,198 @@
+# from sklearn import datasets
+from openTSNE import TSNEEmbedding
+from openTSNE import TSNE
+from openTSNE.affinity import PerplexityBasedNN
+from openTSNE import initialization
+from openTSNE.callbacks import ErrorLogger
+# import TSNE_utils
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pickle
+
+MACOSKO_COLORS = {
+    "Amacrine cells": "#A5C93D",
+    "Astrocytes": "#8B006B",
+    "Bipolar cells": "#2000D7",
+    "Cones": "#538CBA",
+    "Fibroblasts": "#8B006B",
+    "Horizontal cells": "#B33B19",
+    "Microglia": "#8B006B",
+    "Muller glia": "#8B006B",
+    "Pericytes": "#8B006B",
+    "Retinal ganglion cells": "#C38A1F",
+    "Rods": "#538CBA",
+    "Vascular endothelium": "#8B006B",
+}
+ZEISEL_COLORS = {
+    "Astroependymal cells": "#d7abd4",
+    "Cerebellum neurons": "#2d74bf",
+    "Cholinergic, monoaminergic and peptidergic neurons": "#9e3d1b",
+    "Di- and mesencephalon neurons": "#3b1b59",
+    "Enteric neurons": "#1b5d2f",
+    "Hindbrain neurons": "#51bc4c",
+    "Immature neural": "#ffcb9a",
+    "Immune cells": "#768281",
+    "Neural crest-like glia": "#a0daaa",
+    "Oligodendrocytes": "#8c7d2b",
+    "Peripheral sensory neurons": "#98cc41",
+    "Spinal cord neurons": "#c52d94",
+    "Sympathetic neurons": "#11337d",
+    "Telencephalon interneurons": "#ff9f2b",
+    "Telencephalon projecting neurons": "#fea7c1",
+    "Vascular cells": "#3d672d",
+}
+MOUSE_10X_COLORS = {
+    0: "#FFFF00",
+    1: "#1CE6FF",
+    2: "#FF34FF",
+    3: "#FF4A46",
+    4: "#008941",
+    5: "#006FA6",
+    6: "#A30059",
+    7: "#FFDBE5",
+    8: "#7A4900",
+    9: "#0000A6",
+    10: "#63FFAC",
+    11: "#B79762",
+    12: "#004D43",
+    13: "#8FB0FF",
+    14: "#997D87",
+    15: "#5A0007",
+    16: "#809693",
+    17: "#FEFFE6",
+    18: "#1B4400",
+    19: "#4FC601",
+    20: "#3B5DFF",
+    21: "#4A3B53",
+    22: "#FF2F80",
+    23: "#61615A",
+    24: "#BA0900",
+    25: "#6B7900",
+    26: "#00C2A0",
+    27: "#FFAA92",
+    28: "#FF90C9",
+    29: "#B903AA",
+    30: "#D16100",
+    31: "#DDEFFF",
+    32: "#000035",
+    33: "#7B4F4B",
+    34: "#A1C299",
+    35: "#300018",
+    36: "#0AA6D8",
+    37: "#013349",
+    38: "#00846F",
+}
+
+def viz_plot(
+    x,
+    y,
+    ax=None,
+    title=None,
+    draw_legend=True,
+    draw_centers=False,
+    draw_cluster_labels=False,
+    colors=None,
+    legend_kwargs=None,
+    label_order=None,
+    **kwargs
+):
+    import matplotlib
+
+    if ax is None:
+        _, ax = matplotlib.pyplot.subplots(figsize=(8, 8))
+
+    if title is not None:
+        ax.set_title(title)
+
+    plot_params = {"alpha": kwargs.get("alpha", 0.6), "s": kwargs.get("s", 10)}
+
+    # Create main plot
+    if label_order is not None:
+        assert all(np.isin(np.unique(y), label_order))
+        classes = [l for l in label_order if l in np.unique(y)]
+    else:
+        classes = np.unique(y)
+    if colors is None:
+        default_colors = matplotlib.rcParams["axes.prop_cycle"]
+        colors = {k: v["color"] for k, v in zip(classes, default_colors())}
+
+    point_colors = list(map(colors.get, y))
+
+    ax.scatter(x[:, 0], x[:, 1], c=point_colors, rasterized=True, **plot_params)
+
+    # Plot mediods
+    if draw_centers:
+        centers = []
+        for yi in classes:
+            mask = yi == y
+            centers.append(np.median(x[mask, :2], axis=0))
+        centers = np.array(centers)
+
+        center_colors = list(map(colors.get, classes))
+        ax.scatter(
+            centers[:, 0], centers[:, 1], c=center_colors, s=48, alpha=1, edgecolor="k"
+        )
+
+        # Draw mediod labels
+        if draw_cluster_labels:
+            for idx, label in enumerate(classes):
+                ax.text(
+                    centers[idx, 0],
+                    centers[idx, 1] + 2.2,
+                    label,
+                    fontsize=kwargs.get("fontsize", 6),
+                    horizontalalignment="center",
+                )
+
+    # Hide ticks and axis
+    ax.set_xticks([]), ax.set_yticks([]), ax.axis("off")
+
+    if draw_legend:
+        legend_handles = [
+            matplotlib.lines.Line2D(
+                [],
+                [],
+                marker="s",
+                color="w",
+                markerfacecolor=colors[yi],
+                ms=10,
+                alpha=1,
+                linewidth=0,
+                label=yi,
+                markeredgecolor="k",
+            )
+            for yi in classes
+        ]
+        legend_kwargs_ = dict(loc="center left", bbox_to_anchor=(1, 0.5), frameon=False, )
+        if legend_kwargs is not None:
+            legend_kwargs_.update(legend_kwargs)
+        ax.legend(handles=legend_handles, **legend_kwargs_)
+    
+    matplotlib.pyplot.show()
+
+
+if __name__ == '__main__':
+    data_dir = "D:\\2020BUAA\dataset\JNU"
+    pic_data = os.path.join(data_dir,"JNU_data_0-1.pk")
+
+    with open(pic_data, 'rb') as file_1:
+                txt_all_data = pickle.load(file_1)
+                
+    source_train_X,source_train_y = txt_all_data[0]
+    source_val_X,source_val_y  = txt_all_data[1]
+    target_train_X,target_train_y = txt_all_data[2]
+    target_val_X,target_val_y = txt_all_data[3]
+
+    x, y = source_val_X,source_val_y
+
+    tsne = TSNE(
+        perplexity=30,
+        n_iter=100,
+        metric="euclidean",
+        callbacks=ErrorLogger(),
+        n_jobs=8,
+        random_state=42,
+    )
+    embedding = tsne.fit(x)
+    viz_plot(embedding, y, colors=MOUSE_10X_COLORS,draw_centers=False)
